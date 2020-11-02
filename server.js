1c79ab29f738071422f3659ef16de5c5590960c9
Dealbisac <chand.dipendra19@gmail.com>
2020-11-02 14:32:12 +0545

    Database optimized

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 //App Config
 const app = express();
 const port = process.env.PORT || 8002;
 
 //Middlewares
+app.use(express.json());
+app.user(cors());
 
 //DB Config
+const mongoURI = process.env.MONGO_URI || 'test';
+
+mongoose.connect(mongoURI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 //Api Routes
 app.get('/', (req, res) => res.status(200).send('Hello World'));
