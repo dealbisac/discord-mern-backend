e898a2cde56739a145c484668a95d2bccdc0fa64
Dealbisac <chand.dipendra19@gmail.com>
2020-11-02 13:08:34 +0545

    Local Server Setup

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import mongooser from 'mongoose';
+import mongoose from 'mongoose';
 
 //App Config
+const app = express();
+const port = process.env.PORT || 8002;
 
 //Middlewares
 
 //DB Config
 
 //Api Routes
+app.get('/', (req, res) => res.status(200).send('Hello World'));
 
 //Listener
-
+app.listen(port, () => console.log(`Listening on localhost at port: ${port}`));
